69c47f793697c9b4a093d4aa02ef264fc4988e36
citli <citlima29@gmail.com>
2026-02-11 14:07:26 +0000

    Initial Angular setup with the two containers to show

---
+++ src/app/page/page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page',
+  imports: [],
+  templateUrl: './page.html',
+  styleUrl: './page.css',
+})
+export class Page {
+
+}
