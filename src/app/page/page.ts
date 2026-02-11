a435e26f6f2276c963bffc0b9eb1b3573406a6d4
citli <citlima29@gmail.com>
2026-02-11 15:33:28 +0000

    added the mockup for the page displayer

--- src/app/page/page.ts
+++ src/app/page/page.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'page',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './page.html',
   styleUrl: './page.css',
 })
 export class Page {
-
+  protected readonly page_title = signal('dd_mm_yyyy.md');
+  protected readonly page_content = signal('Once upon a time...\nJoe Mama was having fun...\nAnd then stopped. :c\n');
 }
