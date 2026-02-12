109fc5917115f7f6438d9f83accf3e99a44664f0
citli <citlima29@gmail.com>
2026-02-12 12:22:31 +0000

    can make http calls to api/page, need to make the localhost not hardcoded

--- src/app/page/page.ts
+++ src/app/page/page.ts
@@ -1,13 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient, HttpRequest } from '@angular/common/http';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from '../api-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'page',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './page.html',
   styleUrl: './page.css',
 })
 export class Page {
   protected readonly page_title = signal('dd_mm_yyyy.md');
   protected readonly page_content = signal('Once upon a time...\nJoe Mama was having fun...\nAnd then stopped. :c\n');
+
+  private api = inject(ApiService);
+
+  page$ = this.api.getPage();
+  async = "";
 }
